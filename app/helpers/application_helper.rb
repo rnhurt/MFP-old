7379a083959c64108db6f114c6cbd04c0662cefd
Richard Hurt <rnhurt@kangaroobox.com>
2010-05-28 22:30:13 -0400

    Adde jQuery-UI and basic layout look&feel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,28 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	# Generate the MENU html.
+	def menu_builder(page_id)
+		content = ''
+
+    ['people', 'incidents', 'vehicles', 'reports'].each do |page|
+      content << if page_id == page
+        content_tag( :li, link_to(page.humanize, page , :class => 'ui-state-default ui-corner-top ui-tabs-selected ui-state-active'))
+      else
+        content_tag( :li, link_to(page.humanize, page , :class => 'ui-state-default ui-corner-top'))
+      end
+    end
+
+    return content
+	end
+
+  
+  # Show the FLASH div if there is data in the flash object.
+	def show_flash
+		result = ''
+		flash.each {|type, message| result << content_tag(:div, message, :id => 'notice', :class => type.to_s) }
+		return result
+	end
+
 end
+
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Begin processing jQuery commands after the page loads
+$(function() {
+//  $('#date').datepicker();          // Initialize the date picker elements
+
+  //  UIInit();                       // Set up the user interface components
+  tabInit();                      // Initialize the tab elements
+  //  accordionInit();                // Initialize the accordian elements
+  //  calculatorInit();               // Initialize the calculator elements
+  //  thickboxInit();                 // Initialize the ThinkBox elements
+  $("input.focus:last").focus();  // Set the focus on the last input tag with a class of "focus"
+});
+
+
+function tabInit(){
+  $("#tabs").tabs({
+    //cookie: {expires: 1}
+  });
+}
