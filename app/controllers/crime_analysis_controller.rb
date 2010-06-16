55bf440f6511d3a983e161e39836a248b072bfd8
Richard Hurt <rnhurt@gmail.com>
2010-06-16 03:34:07 -0500

    Reworked soem SQL to work better on Postgres; its still evil and needs to be excised though.  :(

--- app/controllers/crime_analysis_controller.rb
+++ app/controllers/crime_analysis_controller.rb
@@ -1,8 +1,10 @@
 class CrimeAnalysisController < ApplicationController
 
   def index
-    @report_types = Report.connection.execute("SELECT codes.value, count(*) count FROM codes INNER JOIN reports ON reports.offence_id = codes.id GROUP BY codes.id ORDER BY count DESC")
-    @report_years = Report.connection.execute("SELECT strftime('%Y',updated_at), count(*) count from reports GROUP BY  strftime('%Y',updated_at) ORDER BY strftime('%Y',updated_at) DESC;")
+    # FIXME: These SQL statements should really be re-written as ActiveRecord calls
+    @report_types = Report.connection.execute("SELECT codes.value, count(*) AS count FROM codes INNER JOIN reports ON reports.offence_id = codes.id GROUP BY codes.value ORDER BY count DESC;")
+    @report_years = Report.connection.execute("SELECT strftime('%Y',updated_at), count(*) AS count from reports GROUP BY  strftime('%Y',updated_at) ORDER BY strftime('%Y',updated_at) DESC;")
+    # FIXME: Postgres syntax = SELECT extract(year FROM updated_at) AS year, count(*) AS count from reports GROUP BY year ORDER BY year DESC;
   end
 
 end
