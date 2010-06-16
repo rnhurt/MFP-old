790807cadbc44a9fd4cd37b17630ce9e8728bb0f
Richard Hurt <rnhurt@gmail.com>
2010-06-16 06:57:10 -0500

    Migrating raw SQL to AR calls...

--- app/controllers/crime_analysis_controller.rb
+++ app/controllers/crime_analysis_controller.rb
@@ -1,10 +1,17 @@
 class CrimeAnalysisController < ApplicationController
 
   def index
-    # FIXME: These SQL statements should really be re-written as ActiveRecord calls
-    @report_types = Report.connection.execute("SELECT codes.value, count(*) AS count FROM codes INNER JOIN reports ON reports.offence_id = codes.id GROUP BY codes.value ORDER BY count DESC;")
+    @offence_counts = Offence.find(:all, :joins => :reports,  
+      :select => 'codes.*, count(*) AS count', 
+      :group => 'codes.value',
+      :order => 'count DESC')
     @report_years = Report.connection.execute("SELECT strftime('%Y',updated_at), count(*) AS count from reports GROUP BY  strftime('%Y',updated_at) ORDER BY strftime('%Y',updated_at) DESC;")
     # FIXME: Postgres syntax = SELECT extract(year FROM updated_at) AS year, count(*) AS count from reports GROUP BY year ORDER BY year DESC;
+
+    @report_counts_by_year = Offence.find(:all, :joins => :reports,
+      :select => 'codes.*, count(*) AS count',
+      :group => 'updated_at',
+      :order => 'count DESC')
   end
 
 end
