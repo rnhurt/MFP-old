905bc97f1bb8a0f3b96c623b960bcb7c82bac98e
Richard Hurt <rnhurt@gmail.com>
2010-06-17 08:29:13 -0500

    modified to use Google static maps until I figure out some issues with the Google Map API key

--- app/controllers/crime_analysis_controller.rb
+++ app/controllers/crime_analysis_controller.rb
@@ -11,6 +11,14 @@ class CrimeAnalysisController < ApplicationController
       blah = Time.now - i.years
       @report_counts_by_year << [blah.year, Report.count(:all, :conditions => { :updated_at => blah.beginning_of_year..blah.end_of_year })]
     end
+
+    @locations = Report.recent.collect{|i| i.location.full_address}
+
+    @map_url = "http://maps.google.com/maps/api/staticmap?sensor=false"
+    @map_url << "&markers=color:green|label:R|#{@locations[0..3].join('|')}"
+    @map_url << "&markers=color:red|label:G|#{@locations[4..7].join('|')}"
+    @map_url << "&markers=color:blue|label:B|#{@locations[8..10].join('|')}"
+
   end
 
 end
