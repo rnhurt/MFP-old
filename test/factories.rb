58ec7ecd6b254242acdf68adf199e03ad28da72d
Richard Hurt <rnhurt@kangaroobox.com>
2010-07-05 16:00:24 -0400

    Began adding tests with Factory Girl integration

---
+++ test/factories.rb
@@ -0,0 +1,10 @@
+Factory.define :user do |f|
+  f.sequence(:login) {|n| "foo#{n}" }
+  f.password  'foobar'
+  f.password_confirmation {|u| u.password}
+  f.sequence(:email) {|n| "foo#{n}@example.com" }
+end
+
+Factory.define :person do |f|
+
+end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+	def test_user_first_name_limits
+    user = Factory.build(:user, :first_name => 'This is a really long name that probably shouldn\'t be legal because it is too long')
+		assert !user.valid?, 'First Name too long'
+
+		user = Factory.build(:user, :first_name => '')
+		assert !user.valid?, 'First Name too short'
+
+		user = Factory.build(:user)
+		assert !user.valid?, 'First Name just right'
+	end
+
+	def test_user_last_name_limits
+    user = Factory.build(:user, :last_name => 'This is a really long name that probably shouldn\'t be legal because it is too long')
+		assert !user.valid?, 'Last Name too long'
+
+		user = Factory.build(:user, :last_name => '')
+		assert !user.valid?, 'Last Name too short'
+
+		user = Factory.build(:user)
+		assert !user.valid?, 'Last Name just right'
+	end
+
+  def test_user_login_limits
+    user = Factory.build(:user, :login => 'This is a really long name that probably shouldn\'t be legal because it is too long')
+		assert !user.valid?, 'Login too long'
+
+		user = Factory.build(:user, :login => '')
+		assert !user.valid?, 'Login too short'
+
+		user = Factory.build(:user)
+		assert !user.valid?, 'Login just right'
+	end
+
 end
